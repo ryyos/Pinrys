bfbba7c60bb6b7a53ce2d90fbaf63a5ae32ffc79
ryosyntaxx <riodwi12174@gmail.com>
2023-12-24 00:56:39 +0700

    add create param and prepare change flow

--- libs/service/parser.py
+++ libs/service/parser.py
@@ -1,23 +1,29 @@
 import requests
-
+from urllib.parse import urlencode
 from requests import Response
 from fake_useragent import FakeUserAgent
 from datetime import datetime
 from time import time, sleep
 from icecream import ic
 from typing import Union, List, Dict, Any
+from json import dumps
 
 from libs.helpers.Writer import Writer
+from libs.utils.ApiRetry import ApiRetry
 class Parser:
     def __init__(self) -> None:
         self.__writer = Writer()
         self.__user_agent = FakeUserAgent()
+        self.__retry = ApiRetry()
         self.__api = 'https://id.pinterest.com/resource/BaseSearchResource/get/'
 
         self.__headers = {
             "User-Agent": self.__user_agent.random
         }
-        self.__payload = {
+
+
+    def __create_param(self, query: str, size: int) -> dict:
+        return {
             "options": {
                 "article": "",
                 "appliedProductFilters": "---",
@@ -26,70 +32,39 @@ class Parser:
                 "scope": "pins",
                 "auto_correction_disabled": "",
                 "top_pin_id": "",
-                "filters": ""
+                "filters": "",
+                "query": query,
+                "page_size": size
             }
         }
 
 
-    def __retry(self, url: str, max_retries: int= 5, retry_interval: Union[int, float] = 0.2) -> Response :
-
-        def get():
-            for _ in range(max_retries):
-                try:
-                    response = requests.get(url=url, headers=self.__headers)
-                    ic(response)
-                    return response
-                except Exception as err:
-                    # self.__logs.err(message=err, url=url)
-                    ic(err)
-                sleep(retry_interval)
-                retry_interval+= 0.2
-            return response
-        
-        def post():
-            for _ in range(max_retries):
-                try:
-                    response = requests.post(url=url, headers=self.__headers)
-                    ic(response)
-                    return response
-                except Exception as err:
-                    # self.__logs.err(message=err, url=url)
-                    ic(err)
-                sleep(retry_interval)
-                retry_interval+= 0.2
-            return response
-
-
-    def __next_steps(self, bookmark: str) -> List(Dict["str", Any]):
-        pass
-
     def execute_query(self, name: str) -> dict:
-        self.__payload["query"] = name
 
-        response: Response = self.__retry(url=f'{self.__api}?source_url=/search/pins/?q={name}&rs=typed&{str(self.__payload)}')
+        response: Response = self.__retry.get(url=f'{self.__api}?source_url=/search/pins/?q={name}&rs=typed&data={str(self.__create_param(query=name))}', headers=self.__headers)
         resource: dict = response.json()["resource_response"]
         
-        results = {
-            "status": resource["status"],
-            "message": resource["message"],
-            "total": len(resource["data"]["results"]),
-            "data": [
-                {
-                    "description": data["description"],
-                    "author": {
-                        "name": data["pinner"]["full_name"],
-                        "username": data["pinner"]["username"],
-                        "followers": data["pinner"]["follower_count"]
-                    },
-                    "created": data["created_at"],
-                    "title": data["title"],
-                    "content_domain": data["domain"],
-                    "images": data["images"]
-                } for data in resource["data"]["results"]
-            ]
-        }
+        # results = {
+        #     "status": resource["status"],
+        #     "message": resource["message"],
+        #     "total": len(resource["data"]["results"]),
+        #     "data": [
+        #         {
+        #             "description": data["description"],
+        #             "author": {
+        #                 "name": data["pinner"]["full_name"],
+        #                 "username": data["pinner"]["username"],
+        #                 "followers": data["pinner"]["follower_count"]
+        #             },
+        #             "created": data["created_at"],
+        #             "title": data["title"],
+        #             "content_domain": data["domain"],
+        #             "images": data["images"]
+        #         } for data in resource["data"]["results"]
+        #     ]
+        # }
 
-        self.__writer.write_json(path=f'data/results.json', content=results)
+        # self.__writer.write_json(path=f'data/results.json', content=results)
 
 
     def main(self, name: str):
@@ -111,8 +86,6 @@ class Parser:
 
         # self.__writer.ex(path=f'data/{name.replace(" ", "_")}.json', content=response.json())
         bookmark = response.json()["resource_response"]["bookmark"]
-    def next_steps(self, name: str):
-        response = requests.post(url=self.__api, headers=self.__headers)
 
 
 if __name__ == '__main__':
--- libs/utils/ApiRetry.py
+++ libs/utils/ApiRetry.py
@@ -1,17 +1,36 @@
+import requests
+from requests import Response
 from typing import Union
 from time import sleep
+from icecream import ic
+
 
 class ApiRetry:
-    def __init__(self, url: str, headers: dict, max_retries: int = 5, retry_interval: Union[int, float] = 0.2):
-        self.url = url
-        self.headers = headers
-        self.max_retries = max_retries
-        self.retry_interval = retry_interval
-
-    def get(self):
-        # Implementasi logika retry untuk HTTP GET
+    def __init__(self) -> None:
         pass
 
-    def post(self):
-        # Implementasi logika retry untuk HTTP POST
-        pass
+    def get(self, url: str, headers: dict, max_retries: int = 5, retry_interval: Union[int, float] = 0.2):
+        for _ in range(max_retries):
+            try:
+                response = requests.get(url=url, headers=headers)
+                ic(response)
+                return response
+            except Exception as err:
+                # self.__logs.err(message=err, url=url)
+                ic(err)
+            sleep(retry_interval)
+            retry_interval+= 0.2
+        
+
+    def post(self, url: str, headers: dict, max_retries: int = 5, retry_interval: Union[int, float] = 0.2, payload: any = None):
+        for _ in range(max_retries):
+            try:
+                response = requests.post(url=url, headers=headers, data=payload)
+                ic(response)
+                return response
+            except Exception as err:
+                # self.__logs.err(message=err, url=url)
+                ic(err)
+            sleep(retry_interval)
+            retry_interval+= 0.2
+        
